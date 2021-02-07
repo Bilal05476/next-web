ab2849c68c522a7fa9b2d8b59960cdebc9ba50a9
Bilal Ahmed <55283639+Bilal05476@users.noreply.github.com>
2021-02-07 23:15:04 +0500

    next-app

---
+++ comps/Navbar.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className='logo'>
+        <Image src='/weblogo.png' width={120} height={120} />
+      </div>
+      <Link href='/'>
+        <a>Home</a>
+      </Link>
+      <Link href='/about'>
+        <a>About</a>
+      </Link>
+      <Link href='/ninjas/'>
+        <a>Ninja Listing</a>
+      </Link>
+    </nav>
+  );
+};
+
+export default Navbar;
